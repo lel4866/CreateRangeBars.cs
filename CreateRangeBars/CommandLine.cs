a3ba67475baffc7537e08ed27944a2666b4676bf
Larry Lewis <lel486@gmail.com>
2021-09-01 08:07:28 -0400

    add command line processing

---
+++ CreateRangeBars/CommandLine.cs
@@ -0,0 +1,58 @@
+﻿//
+// static class CommandLine
+// Process CreateRangeBars command line
+//
+
+namespace ReadSierraChartDataSharp;
+
+static class CommandLine {
+    internal static void ProcessCommandLineArguments(string[] args) {
+        string? arg_name = null;
+
+        foreach (string arg in args) {
+            if (arg_name == null) {
+                switch (arg) {
+                    case "-v":
+                    case "--version":
+                        Console.WriteLine(Program.version);
+                        break;
+                    case "-r":
+                    case "--replace":
+                        Program.update_only = false;
+                        Console.WriteLine(Program.version);
+                        break;
+                    case "-s":
+                    case "--symbol":
+                        arg_name = "-s";
+                        break;
+                    case "-h":
+                    case "--help":
+                        Console.WriteLine(Program.version);
+                        Console.WriteLine("Create range bar files from csv tick files");
+                        Console.WriteLine("Command line arguments:");
+                        Console.WriteLine("    --version, -v : display version number");
+                        Console.WriteLine("    --update, -u  : only process files input directory which do not have corresponding file in output directory");
+                        Console.WriteLine("    --symbol, -s  : futures contract symbol; i.e. for CME SP500 e-mini: ES");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid command line argument: " + arg);
+                        System.Environment.Exit(-1);
+                        break;
+                }
+            }
+            else {
+                switch (arg_name) {
+                    case "-s":
+                        if (Program.futures_root.Length > 3) {
+                            Console.WriteLine("Invalid futures contract symbol: " + arg);
+                            System.Environment.Exit(-1);
+                        }
+                        Program.futures_root = arg.ToUpper();
+                        break;
+                }
+                arg_name = null;
+            }
+        }
+    }
+}
